8dbf55e76369f3a629bc1f17104e2e754234af39
AdityaKate2003 <adityakate9545@gmail.com>
2026-07-16 12:23:53 +0530

    fix(android): dynamically inject namespace for legacy Gradle plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,8 +19,27 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureNamespace = {
+        val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (android != null && android.namespace == null) {
+            android.namespace = "com.example.${name.replace(":", "").replace("-", "_")}"
+        }
+    }
+    if (state.executed) {
+        configureNamespace()
+    } else {
+        afterEvaluate {
+            configureNamespace()
+        }
+    }
+}
+
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+
 
 
