1ce9becabe2dbbbadbc3d96938a5fb7d08bb793b
AdityaKate2003 <adityakate9545@gmail.com>
2026-07-17 20:38:49 +0530

    fsdhgf

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -18,9 +18,9 @@ import '../screens/auth/autoblocked_warning_screen.dart';
 
 class ApiService {
   // Toggle this to true to use your local backend, false for production
-  static const bool isLocal = false;
+  static const bool isLocal = true;
 
-  // Uses your machine's local IP (192.168.0.205) for local dev on a real device
+  // Uses your machine's local IP (192.168.0.150) for local dev on a real device
   static String get baseUrl {
     if (!isLocal) {
       return 'https://lunara-api-server-a8gfdvg0hjdec6gx.centralindia-01.azurewebsites.net';
@@ -28,7 +28,7 @@ class ApiService {
     if (kIsWeb) {
       return 'http://localhost:9076';
     }
-    return 'http://localhost:9076';
+    return 'http://192.168.0.150:9076';
   }
 
   static String? _authToken;
