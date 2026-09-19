2e3cf0fa1a9c05cd5b5d5b0cd6c9c301818043b7
AdityaKate2003 <adityakate9545@gmail.com>
2026-09-19 16:47:51 +0530

    Update backend API endpoint and deployment target to new Azure account

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -158,7 +158,7 @@ class ApiService {
   // Uses your machine's local IP (192.168.0.154) for local dev on a real device
   static String get baseUrl {
     if (!isLocal) {
-      return 'https://lunara-api-v2-b2cnahe5bzcxfdds.centralindia-01.azurewebsites.net';
+      return 'https://lunara-backend-api-e3hhhmc2g0c2hgd0.centralindia-01.azurewebsites.net';
     }
     if (kIsWeb) {
       return 'http://localhost:9076';
